0d9cdc1b2d0c7b65d90b097f0bd12fe949e485de
CrucifiedOnTheCross <145364518+CrucifiedOnTheCross@users.noreply.github.com>
2024-11-05 17:14:30 +0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,29 +1,8 @@
-def f(n):
-    if n % 2 == 0:
-        return False
-
-    for i in range(3, int(n ** 0.5) + 1, 2):
-        if n % i == 0:
-            return False
-
-    return True
-
-
 n = int(input())
-if f(n) == True:
-    print('Prime')
+
+for i in range(2, n):
+    if n % i == 0:
+        print("compose")
+        break
 else:
-    print('Composite')
-
-M = [[1, 2, 3],
-     [5, 6, 7],
-     [8, 9, 0]]
-
-# l - строка матрицы M
-for l in M:
-    # e - элемент строки l
-    for e in l:
-        # вывыоди элемент матрицы, так, чтобы каждый элемент отделялся пробелом друг от друга
-        print(e, end=' ')
-    # переходим на новую строку
-    print()
+    print("prime")
